dcc61670c12f2ccb12119f42b56b377c79631d49
Fiodor Gorobet <casusarts@gmail.com>
2017-02-17 17:40:04 +0100

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+
+  // var data = {
+  //   "Content-Type": "application/x-www-form-urlencoded",
+  //   "X-Mashape-Key": "7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ",
+  //   "Accept": "application/json"
+  // }
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/x-www-form-urlencoded",
+      "x-mashape-key": "7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ",
+      "accept": "application/json",
+      "cache-control": "no-cache",
+      "postman-token": "878ad831-4f8f-d6cb-54ae-f900a8943fd4"
+    }
+  }
+
+
+
+  $("#getQuote").on("click load", function() {
+
+    $.ajax(settings).done(function(res) {
+
+      var html = "";
+
+        // console.log(res);
+        // console.log(loc);
+
+      res = JSON.parse(res);
+        // console.log(res);
+        // console.log(res.author);
+        // console.log(res.quote);
+
+      html += "<cite>" + res.quote + "</cite><br>";
+      html += "<strong> - " + res.author + "</strong><br>";
+      html += "<i>Category: " + res.category + "</i>"
+
+      $("#quote").html(html);
+    });
+  });
+
+
+
+});
+//  7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ
