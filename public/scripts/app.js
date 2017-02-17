03295a4e04029107fc0c28d0adbf55cb044f6545
Fiodor Gorobet <casusarts@gmail.com>
2017-02-17 18:30:45 +0100

    refactor app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,10 +13,8 @@ $(document).ready(function() {
     }
   }
 
-
-
-  $("#getQuote").on("click load", function() {
-
+  // Getting Quotes from https://market.mashape.com/andruxnet/random-famous-quotes API
+  function fetchQuote(e) {
     $.ajax(settings).done(function(res) {
 
       var html = "";
@@ -29,9 +27,17 @@ $(document).ready(function() {
 
       $("#quote").html(html);
     });
+  }
+
+  // Show "Loading..." in <div id="quote>... while content is loading.
+  $(window).on('load', function() {
+    $("#quote").html("<i>Loading...</i>");
   });
 
+  // Show initial quote on application is beeng loaded
+  $(window).load(fetchQuote);
 
-
+  // Show next random quote
+  $("#getQuote").on("click", fetchQuote);
 });
 
