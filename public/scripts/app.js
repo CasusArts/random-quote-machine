2aa6c10eec8401964c7844268145b72647c203c9
Fiodor Gorobet <casusarts@gmail.com>
2017-02-17 18:38:15 +0100

    hide settings obj from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
 
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
-    "method": "POST",
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-      "x-mashape-key": "7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ",
-      "accept": "application/json",
-      "cache-control": "no-cache"
-    }
-  }
+
 
   // Getting Quotes from https://market.mashape.com/andruxnet/random-famous-quotes API
   function fetchQuote(e) {
