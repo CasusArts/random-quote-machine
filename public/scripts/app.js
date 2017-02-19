73b34856e9968fdab1ea965f9f04c88fb079a8ed
Fiodor Gorobet <casusarts@gmail.com>
2017-02-19 13:24:58 +0100

    tweeaking on styles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,43 @@
-// Getting Quotes from https://market.mashape.com/andruxnet/random-famous-quotes API
-function getQuote(e) {
-  $.ajax(settings).done(function(res) {
+  // Getting Quotes from https://market.mashape.com/andruxnet/random-famous-quotes API
+  function getQuote(e) {
+    $.ajax(settings).done(function(res) {
 
-    var html = "";
+      var html = "";
 
-    res = JSON.parse(res);
+      res = JSON.parse(res);
 
-    var quote = res.quote;
-    var author = res.author;
+      var quote = res.quote;
+      var author = res.author;
 
-    html += "<cite>" + res.quote + "</cite><br>";
-    html += "<strong> - " + res.author + "</strong><br>";
+      html += "<p><cite class='quote'><span class='quote-sign'>&#8220;</span>" + res.quote + "<span class='quote-sign'>&#8221;</span></cite></p>";
+      html += "<p class='author'><strong> - " + res.author + "</strong></p>";
 
-    // TODO: Enhance multiple category selection
-    // html += "<i>Category: " + res.category + "</i>"
+      // TODO: Enhance multiple category selection
+      // html += "<i>Category: " + res.category + "</i>"
 
-    $("#quote").html(html);
+      $("#quote").html(html);
 
-    $('.twitter-share-button')
-      .attr(
-        'href',
-        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
-        + encodeURIComponent('"' + quote + '" ' + author)
-      );
+      $('.twitter-share')
+        .attr(
+          'href',
+          'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
+          + encodeURIComponent('"' + quote + '" ' + author)
+        );
+    });
+  }
+
+  $(document).ready(function() {
+
+    // Show "Loading..." in <div id="quote>... while content is loading.
+    $(window).on('load', function() {
+      $("#quote").html("<i>Loading...</i>");
+    });
+
+    // Show initial quote on application is beeng loaded
+    $(window).load(getQuote);
+
+    // Show next random quote
+    $("#getQuote").on("click", getQuote);
+
   });
-}
-
-$(document).ready(function() {
-
-  // Show "Loading..." in <div id="quote>... while content is loading.
-  $(window).on('load', function() {
-    $("#quote").html("<i>Loading...</i>");
-  });
-
-  // Show initial quote on application is beeng loaded
-  $(window).load(getQuote);
-
-  // Show next random quote
-  $("#getQuote").on("click", getQuote);
-
-});
 
