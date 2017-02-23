a7ad084324c9cbf91143e8f9b6a078277a3d1fbf
Fiodor Gorobet <casusarts@gmail.com>
2017-02-23 12:35:06 +0100

    add settings.js

---
+++ public/scripts/settings.js
@@ -0,0 +1,9 @@
+var settings = {
+  "url": "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=", // ?cat=movies (for quering just from movies category)
+  "method": "POST",
+  "headers": {
+    "content-type": "application/x-www-form-urlencoded",
+    "x-mashape-key": "7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ",
+    "accept": "application/json",
+  }
+}
