c63f9d6a69110297db329c63494a2a861648abef
Fiodor Gorobet <casusarts@gmail.com>
2017-02-24 16:49:15 +0100

    change footbar position

---
+++ settings.js
@@ -0,0 +1,12 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://andruxnet-random-famous-quotes.p.mashape.com/", // ?cat=movies (for quering just from movies category)
+  "method": "POST",
+  "headers": {
+    "content-type": "application/x-www-form-urlencoded",
+    "x-mashape-key": "7xuHGaQFVXmsh9hseX7ljiSdyF7ep1HpEbNjsnwfJNXTmSi2CQ",
+    "accept": "application/json",
+    "cache-control": "no-cache"
+  }
+}
