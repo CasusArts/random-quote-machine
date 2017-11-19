3f7c0503d2ac2649077ec24c6b8a3c12088e2a10
Fiodor Gorobet <casusarts@gmail.com>
2017-11-19 13:23:36 +0100

    add fetch api native call without jquery ajax. change api provider to https://talaikis.com/api/quotes/random/

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,50 @@
   // Getting Quotes from https://market.mashape.com/andruxnet/random-famous-quotes API
-  function getQuote(e) {
-    $.ajax(settings).done(function(res) {
+function fetchQuote() {
+  const init = {
+    method: 'GET'
+  };
+  fetch("https://talaikis.com/api/quotes/random/").then(data => {
+    let contentType = data.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return data.json();
+    }
+    throw new TypeError("NOT JSON OBJECT");
+  }).then(json => {
 
-      var html = "";
+    let html = '';
+    const quote = json.quote;
+    const author = json.author;
 
-      res = JSON.parse(res);
+    html += "<p><cite class='quote'><span class='quote-sign'>&#8220;</span>" + quote + "<span class='quote-sign'>&#8221;</span></cite></p>";
+    html += "<p class='author'><strong> - " + author + "</strong></p>";
 
-      var quote = res.quote;
-      var author = res.author;
+    // TODO: Enhance multiple category selection
+    // html += "<i>Category: " + res.category + "</i>"
 
-      html += "<p><cite class='quote'><span class='quote-sign'>&#8220;</span>" + res.quote + "<span class='quote-sign'>&#8221;</span></cite></p>";
-      html += "<p class='author'><strong> - " + res.author + "</strong></p>";
+    $("#quote").html(html);
 
-      // TODO: Enhance multiple category selection
-      // html += "<i>Category: " + res.category + "</i>"
-
-      $("#quote").html(html);
-
-      $('.twitter-share')
-        .attr(
-          'href',
-          'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
-          + encodeURIComponent('"' + quote + '" ' + author)
-        );
-    });
-  }
-
-  $(document).ready(function() {
-
-    // Show "Loading..." in <div id="quote>... while content is loading.
-    $(window).on('load', function() {
-      $("#quote").html("<i>Loading...</i>");
-    });
-
-    // Show initial quote on application is beeng loaded
-    $(window).load(getQuote);
-
-    // Show next random quote
-    $("#getQuote").on("click", getQuote);
-
+    $('.twitter-share')
+      .attr(
+        'href',
+        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
+        + encodeURIComponent('"' + quote + '" ' + author)
+      );
+  }).catch(error => {
+    console.error(error);
   });
+}
+
+$(document).ready(function() {
+
+  // Show "Loading..." in <div id="quote>... while content is loading.
+  $(window).on('load', function() {
+    $("#quote").html("<i>Loading...</i>");
+  });
+
+  // Show initial quote on application is beeng loaded
+  $(window).load(fetchQuote);
+
+  // Show next random quote
+  $("#getQuote").on("click", fetchQuote);
+});
 
